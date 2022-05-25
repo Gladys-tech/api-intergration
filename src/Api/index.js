541e3017e4e45b7ff6a29457907f82f7340590d8
Gladys-tech <gladyskyambadde0@gmail.com>
2022-05-25 20:03:10 +0300

    login

--- src/Api/index.js
+++ src/Api/index.js
@@ -46,7 +46,7 @@ class Api {
 
     // authentication
     static async login(data) {
-        return fetch(`${API_URL}/auth/login`, {
+        return fetch(`${API_URL}/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
